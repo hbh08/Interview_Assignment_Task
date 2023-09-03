f6cc14d462ac63ab74ab0bf615211ad7aa2b3d56
hbh08 <himanshu.bhushan89@gmail.com>
2023-09-03 14:41:49 +0530

    Initial commit

---
+++ Datestring.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Assignment_Task
+{
+    internal class Datestring
+    {
+        static bool isValidDateMonth(string d)
+        {
+            bool res = true;
+
+            string dt = "";
+            string mth = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i < 2)
+                    mth += d[i];
+                else
+                    dt += d[i];
+            }
+
+            int dt1 = Convert.ToInt32(dt);
+            int mt = Convert.ToInt32(mth);
+
+            if (mt > 12 || mt < 1 || dt1 < 1 || dt1 > 31)
+                res = false;
+
+
+            return res;
+        }
+
+        static void Main()
+        {
+            string s1 = "Hdjsh asd2324234jghjsd hjsdg sdhk 12212021 idf32432 32423 d34234jh dfh";
+            string v = "";
+
+            for (int i = 0; i < s1.Length - 1; i++)
+            {
+                char ch = s1[i];
+                if (ch >= '0' && ch <= '9')
+                {
+                    while (ch >= '0' && ch <= '9')
+                    {
+                        ch = s1[i];
+                        v = v + ch;
+                        i++;
+                    }
+
+                    v = v.Trim();
+
+                    if (v.Length == 8 && isValidDateMonth(v))
+                        Console.WriteLine(v + " " + v.Length);
+
+                    v = "";
+                }
+            }
+        }
+    }
+}
---
+++ Startpattern.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Assignment_Task
+{
+    internal class Startpattern
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter value for height");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= h; i++)
+            {
+                for (int m = h; m > i; m--)
+                    Console.Write(" ");
+
+                for (int n = 1; n <= i; n++)
+                    Console.Write("*");
+
+                for (int k = 2; k <= i; k++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
